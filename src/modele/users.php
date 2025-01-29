f0c89d38caed79f120c4e5f84039382344f722fa
Augutin-Erceville <a.erceville@lprs.fr>
2025-01-29 10:07:30 +0100

    Hydate de users.php

---
+++ src/modele/users.php
@@ -0,0 +1,15 @@
+<?php
+class Users{
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+    }
+    public function hydrate(array $donnees){
+        foreach ($donnees as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+}
+?>
