80dc28d4e3df060a86fe0eb3e41cc7fb7a752388
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-01-29 10:06:39 +0100

    Réalisation de l'hydratation pour films.php

---
+++ src/modele/films.php
@@ -0,0 +1,20 @@
+<?php
+class Films
+{
+    public function __construct($donnees)
+    {
+        $this->hydrate($donnees);
+
+    }
+
+    public function hydrate($donnees)
+    {
+        foreach ($donnees as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+
+            }
+        }
+    }
+}
