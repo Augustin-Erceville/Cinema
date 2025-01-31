fdd1765fe2125266587f4f2f00c711039627b75d
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-01-31 10:36:37 +0100

    création modele config.php

---
+++ src/modele/config.php
@@ -0,0 +1,17 @@
+<?php
+class Config
+{
+    private $nomBDD = 'projets_cinema';
+    private $serveur = 'localhost';
+    private $user= 'root';
+    private $password = '';
+    private $bdd;
+    public function __construct()
+    {
+        $this->bdd = new PDO("mysql:dbname=".$this->nomBDD.";host=".$this->serveur, $this->user, $this->password);
+    }
+
+    public function getBdd(){
+        return $this->bdd;
+    }
+}
