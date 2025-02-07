95d20eb921f032223c80158252ad7c8deed5d3e4
H.Sohaib <s.hamdi@lprs.fr>
2025-02-07 09:06:57 +0100

    Mise en page bootstrap pour la page Contact.

--- vue/contact.php
+++ vue/contact.php
@@ -1,14 +1,5 @@
 <?php include ("header.php")?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire - Poser une question ou signaler un problème</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
 <div class="container my-5 p-4 rounded shadow bg-white">
     <h1 class="text-center text-primary mb-4">Formulaire - Poser une question ou signaler un problème</h1>
 
@@ -72,6 +63,5 @@
         charCount.textContent = remaining;
     });
 </script>
-</body>
-</html>
 
+<?php include('footer.php') ?>
