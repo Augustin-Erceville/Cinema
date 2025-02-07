9ada18aad63dbc18e479b9bbed9b09f2983fce2f
H.Sohaib <s.hamdi@lprs.fr>
2025-02-07 09:15:56 +0100

    Mise en page bootstrap pour la page Contact.

--- vue/contact.php
+++ vue/contact.php
@@ -54,7 +54,6 @@
     ?>
 </div>
 
-<!-- Liens vers les fichiers Bootstrap locaux -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 <script src="assets/js/bootstrap.bundle.min.js"></script>
 
