39533722ee9f06f7581477ac55a8ec035667332a
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-02-07 09:37:18 +0100

    Réalisation de la page contenant le catalogue des films dans catalogue.php

--- vue/catalogue.php
+++ vue/catalogue.php
@@ -1,59 +1,27 @@
-<?php include ("header.php")?>
+<?php
+include ("header.php");
+require_once "../src/modele/Films.php";
+require_once "../src/repository/FilmsRepository.php";
+$filmRepository = new FilmsRepository();
+$films = $filmRepository->getFilmsObj();
+
+?>
 
 <div class="row">
-    <h1>Catalogue des Films</h1>
+    <h1>Catalogue des Films </h1>
+    <?php
+    /** @var Films $film */
+    foreach ($films as $film){ ?>
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="<?=$film->getAffiche() ?>" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title"><?=$film->getTitre() ?></h5>
+                    <p class="card-text"><?=$film->getDescription() ?></p>
+                    <a href="#" class="btn btn-primary">Afficher les séances</a>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>
-        </div>
-
-    <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-    <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col">
-        <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    </div>
-
+    <?php } ?>
 </div>
 <?php include('footer.php') ?>
