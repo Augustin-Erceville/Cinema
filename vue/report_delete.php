56460afa8a36362e4191a34985a653979d160bdc
OUHAIMMED_So <s.ouhaimmed@lprs.fr>
2025-02-13 16:32:54 +0100

    Ajouts de la suppression de reports: report_delete.php

--- vue/report_delete.php
+++ vue/report_delete.php
@@ -3,6 +3,7 @@ include('header.php');
 $stmt = $bdd->prepare("DELETE FROM reports WHERE id_report = ?");
 $stmt->execute([$_GET['id']]);
 header("Location: report.php");
+
 exit;
 include('footer.php')
 ?>
