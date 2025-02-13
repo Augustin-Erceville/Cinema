c86d82f82bd682f45d411932d5e6f7e0dcc27051
Soufiane <s.ouhaimmed@lprs.fr>
2025-02-13 23:37:26 +0100

    Correction de la suppression de film: film_delete.php

--- vue/film_delete.php
+++ vue/film_delete.php
@@ -1,5 +1,8 @@
 <?php
 include('header.php');
+require_once "../src/bdd/Config.php";
+$config = new Config();
+$bdd = $config->connexion();
 $stmt = $bdd->prepare("DELETE FROM films WHERE id_film = ?");
 $stmt->execute([$_GET['id']]);
 header("Location: film.php");
