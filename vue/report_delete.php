991cfa450278cb90216226742773756820f4d0bf
Soufiane <s.ouhaimmed@lprs.fr>
2025-02-13 21:41:49 +0100

    Correction de la suppression de report: report_delete.php

--- vue/report_delete.php
+++ vue/report_delete.php
@@ -1,8 +1,11 @@
 <?php
 include('header.php');
+require_once "../src/bdd/Config.php";
+$config = new Config();
+$bdd = $config->connexion();
 $stmt = $bdd->prepare("DELETE FROM reports WHERE id_report = ?");
 $stmt->execute([$_GET['id']]);
-header("Location: report.php");
+header("Location: reports.php");
 
 exit;
 include('footer.php')
